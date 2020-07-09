48bd8b2e889c37955efdc9b3bf7ba38ad1c1e387
rohanN2005 <63569996+rohanN2005@users.noreply.github.com>
2020-07-09 00:11:55 -0700

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,20 @@
+class Log extends BaseClass{
+  constructor(x,y,height,angle){
+    super(x,y,20,height,angle);
+    this.image = loadImage("sprites/wood2.png");
+    Matter.Body.setAngle(this.body, angle);
+  }
+  display(){
+    if(this.body.speed<4){
+      super.display();
+    }
+    else{
+      World.remove(world,this.body)
+      push();
+      this.visibility = this.visibility - 5;
+      tint(255,this.visibility);
+      image(this.image, this.body.position.x,this.body.position.y,50,50)
+      pop();
+    }
+  }
+}
